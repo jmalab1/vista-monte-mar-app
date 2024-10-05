94b150f32ad2f7c514d7c299837d2fee4a1e3970
Jayson Malabanan <jmalab24@gmail.com>
2024-10-04 22:49:22 -0400

    menu button for small devices

--- src/modules/Navbar/index.tsx
+++ src/modules/Navbar/index.tsx
@@ -40,49 +40,60 @@ export const Navbar = () => {
       <div className="flex-1">
         <h1 className="text-2xl font-bold ml-6">Vista Monte Mar</h1>
       </div>
-      <div className="flex-none">
-        <label className="flex cursor-pointer gap-2">
+      <div>
+        <label className="swap swap-rotate">
+          {/* this hidden checkbox controls the state */}
+          <input type="checkbox" className="theme-controller" onClick={toggleTheme} checked={theme === dark} />
+
+          {/* sun icon */}
           <svg
+            className="swap-off h-10 w-10 fill-current"
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="12" cy="12" r="5" />
-            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            viewBox="0 0 24 24">
+            <path
+              d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
-          <input
-            type="checkbox"
-            checked={theme === dark}
-            onClick={toggleTheme}
-            className="toggle theme-controller"
-          />
+
+          {/* moon icon */}
           <svg
+            className="swap-on h-10 w-10 fill-current"
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            viewBox="0 0 24 24">
+            <path
+              d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
-        <ul className="menu menu-horizontal px-1">
+      </div>
+      <div className="flex-none hidden lg:block" >
+        <ul className="flex gap-1 menu menu-horizontal px-1">
           {NAV_MENU.map(({ name, href }) => (
-            <li className="hidden lg:block">
+            <li>
               <Link to={href} className='text-md font-bold'>{name}</Link>
             </li>
           ))}
         </ul>
+      </div>
+      <div className="drawer lg:hidden sm:block w-12 ml-2 mr-2">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label htmlFor="my-drawer-4" className="drawer-button btn bg-base-100">
+            <svg xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 drawer-button">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </label>
+        </div>
+        <div className="drawer-side z-50 opacity-95">
+          <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            {NAV_MENU.map(({ name, href }) => (
+              <li>
+                <Link to={href} className='text-md font-bold'>{name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/sections/HouseRulesSection/index.tsx
+++ src/sections/HouseRulesSection/index.tsx
@@ -1,23 +1,24 @@
 import SectionHeader from '../../components/heading/SectionHeader';
-import rules from './house_rules.json'
+import rules from './house_rules.json';
 import _ from 'lodash';
 
 export const HouseRulesSection = () => {
   return (
     <section className="px-8 pb-20 pt-20 lg:pt-0 bg-base-200 shadow-xl" id="house_rules">
-      <div className="mb-10 grid place-items-center text-center">
+      <div className="mb-10 grid">
         <SectionHeader
           title="House Rules"
         >
-          {rules.map((rule) => (
-            <div className="collapse bg-base-200">
-              <input type="radio" name="my-accordion-1" defaultChecked />
-              <div className="collapse-title text-xl font-medium justify">{rule.title}</div>
-              <div className="">
-                <p>{rule.description}</p>
+          <div className='m-12 space-y-6'>
+            {rules.map((rule) => (
+              <div className="bg-base-100 border-t border-b border-secondary px-4 py-3" role="alert">
+                <p className="font-bold text-nuetral">{rule.title}</p>
+                <p className="text-sm text-nuetral">{rule.description}</p>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
+
         </SectionHeader>
       </div>
     </section>
