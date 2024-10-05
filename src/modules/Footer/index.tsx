e7727da9a0ac865613b7a8761e4b164be5f67d8c
Jayson Malabanan <jmalab24@gmail.com>
2024-10-05 12:32:44 -0400

    footer fix

--- src/modules/Footer/index.tsx
+++ src/modules/Footer/index.tsx
@@ -3,7 +3,7 @@ import VrboLogo from '../Logos/VrboLogo';
 
 export const Footer = () => {
   return (
-    <footer className="footer bg-base-200 text-base-content items-center p-4 shadow-inner">
+    <footer className="footer bg-base-300 text-base-content items-center p-4">
       <aside className="grid-flow-col items-center">
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
       </aside>
