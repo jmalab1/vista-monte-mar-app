52b5d77883f25c5a52dda4416b12c1924aab4fe8
Jayson Malabanan <jmalab24@gmail.com>
2024-10-10 22:23:07 -0400

    color change

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -6,13 +6,13 @@ const NotFound = () => {
         <section className="bg-base-200 ">
             <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
                 <div className="wf-ull lg:w-1/2">
-                    <p className="text-sm font-medium text-info">404 error</p>
+                    <p className="text-lg font-bold text-info">404 error</p>
                     <h1 className="mt-3 text-2xl font-semibold text-nuetral md:text-3xl">Page not found</h1>
                     <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist.</p>
 
                     <div className="flex items-center mt-6 gap-x-3">
                         <Link to={"/"} >
-                            <button className="btn btn-sm btn-secondary">
+                            <button className="btn btn-sm btn-secondary text-base-100">
                                 Home
                             </button>
                         </Link>
