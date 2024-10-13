8effb9ecf598b95fbd8801894974603114616a00
Jayson Malabanan <jmalab24@gmail.com>
2024-10-13 14:55:59 -0400

    update headers

--- src/components/heading/SectionHeader/index.tsx
+++ src/components/heading/SectionHeader/index.tsx
@@ -5,17 +5,21 @@ type TSectionHeader = {
   title: string;
   children?: React.ReactNode;
   centerText?: boolean;
+  textSize?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 };
 
 const SectionHeader: FunctionComponent<TSectionHeader> = ({
   title,
   children,
   centerText,
+  textSize,
 }) => {
   return (
     <>
       <div className={centerText ? 'place-content-center text-center' : ''}>
-        <h1 className="text-secondary text-4xl font-bold mt-10 font-pacifico">
+        <h1
+          className={`text-secondary ${textSize ? 'text-' + textSize : 'text-4xl'} font-bold mt-10 font-pacifico`}
+        >
           {title}
         </h1>
         {children}
--- src/sections/DirectionsSection/index.tsx
+++ src/sections/DirectionsSection/index.tsx
@@ -10,42 +10,57 @@ export const DirectionsSection = () => {
     <section className="px-8 pb-10 bg-base-200 shadow-xl" id="house_rules">
       <div className="mb-10 grid">
         <div className="lg:ml-56 lg:mr-56">
-          <SectionHeader title="Location, Airport and Travel Time">
-            <Paragraph>
-              We are located in Torres del Mar Condominiums in Jaco, Costa Rica
-              on the Central Pacific coast. It is roughly 1.5 hours, depending
-              on traffic, from San Juan Santamaria International Airport (SJO).
-            </Paragraph>
-            <Button
-              title={'Directions'}
-              type={'button'}
-              placeholder={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
+          <SectionHeader title="Directions and How to Get there">
+            <SectionHeader
+              title="Location, Airport and Travel Time"
+              textSize="2xl"
             >
-              <Link
-                to={`https://www.google.com/maps/dir/?api=1&origin=SJO&destination=Condominio+Torres+del+Mar`}
-                target="_blank"
+              <Paragraph>
+                We are located in Torres del Mar Condominiums in Jaco, Costa
+                Rica on the Central Pacific coast. It is roughly 1.5 hours,
+                depending on traffic, from San Juan Santamaria International
+                Airport (SJO).
+              </Paragraph>
+              <Button
+                title={'Directions'}
+                type={'button'}
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
               >
-                Direction{' '}
-                <FontAwesomeIcon icon={faDiamondTurnRight} size="xl" />
-              </Link>
-            </Button>
-          </SectionHeader>
-          <SectionHeader title="Navigation">
-            <Paragraph>
-              We recommend using Waze to navigate around Costa Rica. It tends to
-              be more accurate and up-to-date than Google map in road delays and
-              routes around Costa Rica.
-            </Paragraph>
-          </SectionHeader>
-          <SectionHeader title="Transportation">
-            <Paragraph>
-              At the airport you can get a taxi in the arrival area. Below are
-              alternative airport transfer company/contact you could arrange
-              transfer with. We would be happy to also assist in arranging
-              transfer - just let us know.
-            </Paragraph>
+                <Link
+                  to={`https://www.google.com/maps/dir/?api=1&origin=SJO&destination=Condominio+Torres+del+Mar`}
+                  target="_blank"
+                >
+                  Direction{' '}
+                  <FontAwesomeIcon icon={faDiamondTurnRight} size="xl" />
+                </Link>
+              </Button>
+            </SectionHeader>
+            <SectionHeader title="Navigation" textSize="2xl">
+              <Paragraph>
+                We recommend using Waze to navigate around Costa Rica. It tends
+                to be more accurate and up-to-date than Google map in road
+                delays and routes around Costa Rica.
+              </Paragraph>
+            </SectionHeader>
+            <SectionHeader title="Transportation" textSize="2xl">
+              <Paragraph>
+                At the airport you can get a taxi in the arrival area. Below are
+                alternative airport transfer company/contact you could arrange
+                transfer with. We would be happy to also assist in arranging
+                transfer - just let us know.
+              </Paragraph>
+              <ul>
+                <li>
+                  CR Travels & Tours: + 506-6019-3784,
+                  crtravelsandtour@gmail.com
+                </li>
+                <li>Tranfers & Tours Costa Rica: +506-8858-8333</li>
+                <li>Arturo Saenz-Garcia: +506-8822-1921</li>
+                <li>Rich Coast Connections - +506-8309-4521</li>
+              </ul>
+            </SectionHeader>
           </SectionHeader>
         </div>
       </div>
