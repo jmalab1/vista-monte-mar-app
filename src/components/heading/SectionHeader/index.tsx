e402ed3734679af27e1468a3d27c89d677a20f3c
Jayson Malabanan <jmalab24@gmail.com>
2024-10-13 07:34:33 -0400

    update font

--- src/components/heading/SectionHeader/index.tsx
+++ src/components/heading/SectionHeader/index.tsx
@@ -15,7 +15,7 @@ const SectionHeader: FunctionComponent<TSectionHeader> = ({
   return (
     <>
       <div className={centerText ? 'place-content-center text-center' : ''}>
-        <h1 className="text-secondary text-4xl font-bold mt-10">{title}</h1>
+        <h1 className="text-secondary text-4xl font-bold mt-10 font-pacifico">{title}</h1>
         {children}
       </div>
     </>
