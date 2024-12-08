ff079fd50895796889e848b6e42f06afa2d288c6
Jayson Malabanan <jmalab24@gmail.com>
2024-12-08 08:41:53 -0500

    moving items

--- src/sections/GallerySection/index.tsx
+++ src/sections/GallerySection/index.tsx
@@ -1,59 +1,147 @@
 import SectionHeader from '../../components/heading/SectionHeader';
-import image1 from './images/image (1).jpg?w=800&webp';
-import image2 from './images/image (2).jpg?w=800&webp';
-import image3 from './images/image (3).jpg?w=800&webp';
-import image4 from './images/image (4).jpg?w=800&webp';
-import image5 from './images/image (5).jpg?w=800&webp';
-import image6 from './images/image (6).jpg?w=800&webp';
-import image7 from './images/image (7).jpg?w=800&webp';
-import image8 from './images/image (8).jpg?w=800&webp';
-import image9 from './images/image (9).jpg?w=800&webp';
-import image10 from './images/image (10).jpg?w=800&webp';
-import image11 from './images/image (11).jpg?w=800&webp';
-import image12 from './images/image (12).jpg?w=800&webp';
-import image13 from './images/image (13).jpg?w=800&webp';
-import image14 from './images/image (14).jpg?w=800&webp';
-import image15 from './images/image (15).jpg?w=800&webp';
-import image16 from './images/image (16).jpg?w=800&webp';
-import image17 from './images/image (17).jpg?w=800&webp';
-import image18 from './images/image (18).jpg?w=800&webp';
-import image19 from './images/image (19).jpg?w=800&webp';
-import image20 from './images/image (20).jpg?w=800&webp';
-import image21 from './images/image (21).jpg?w=800&webp';
-import image22 from './images/image (22).jpg?w=800&webp';
-import image23 from './images/image (23).jpg?w=800&webp';
-import image24 from './images/image (24).jpg?w=800&webp';
-import image25 from './images/image (25).jpg?w=800&webp';
-import image26 from './images/image (26).jpg?w=800&webp';
-import image27 from './images/image (27).jpg?w=800&webp';
-import image28 from './images/image (28).jpg?w=800&webp';
-import image29 from './images/image (29).jpg?w=800&webp';
-import image30 from './images/image (30).jpg?w=800&webp';
-import image31 from './images/image (31).jpg?w=800&webp';
-import image32 from './images/image (32).jpg?w=800&webp';
-import image33 from './images/image (33).jpg?w=800&webp';
+import balcony1 from '/src/assets/balcony/image (1).jpg?w=800&webp';
+import balcony2 from '/src/assets/balcony/image (2).jpg?w=800&webp';
+import balcony3 from '/src/assets/balcony/image (3).jpg?w=800&webp';
+import lr1 from '/src/assets/living_room/image (1).jpg?w=800&webp';
+import lr2 from '/src/assets/living_room/image (2).jpg?w=800&webp';
+import lr3 from '/src/assets/living_room/image (3).jpg?w=800&webp';
+import lr4 from '/src/assets/living_room/image (4).jpg?w=800&webp';
+import lr5 from '/src/assets/living_room/image (5).jpg?w=800&webp';
+import lr6 from '/src/assets/living_room/image (6).jpg?w=800&webp';
+import lr7 from '/src/assets/living_room/image (7).jpg?w=800&webp';
+import lr8 from '/src/assets/living_room/image (8).jpg?w=800&webp';
+import k1 from '/src/assets/kitchen/image (1).jpg?w=800&webp';
+import k2 from '/src/assets/kitchen/image (2).jpg?w=800&webp';
+import k3 from '/src/assets/kitchen/image (3).jpg?w=800&webp';
+import k4 from '/src/assets/kitchen/image (4).jpg?w=800&webp';
+import k5 from '/src/assets/kitchen/image (5).jpg?w=800&webp';
+import br1 from '/src/assets/bedrooms/image (1).jpg?w=800&webp';
+import br2 from '/src/assets/bedrooms/image (2).jpg?w=800&webp';
+import br3 from '/src/assets/bedrooms/image (3).jpg?w=800&webp';
+import br4 from '/src/assets/bedrooms/image (4).jpg?w=800&webp';
+import br5 from '/src/assets/bedrooms/image (5).jpg?w=800&webp';
+import bt1 from '/src/assets/bathrooms/image (1).jpg?w=800&webp';
+import bt2 from '/src/assets/bathrooms/image (2).jpg?w=800&webp';
+import bt3 from '/src/assets/bathrooms/image (3).jpg?w=800&webp';
+import n1 from '/src/assets/nature/image (1).jpg?w=800&webp';
+import n2 from '/src/assets/nature/image (2).jpg?w=800&webp';
+import n3 from '/src/assets/nature/image (3).jpg?w=800&webp';
+import c1 from '/src/assets/common/image (1).jpg?w=800&webp';
+import c2 from '/src/assets/common/image (2).jpg?w=800&webp';
+import m1 from '/src/assets/misc/image (1).jpg?w=800&webp';
+import m2 from '/src/assets/misc/image (2).jpg?w=800&webp';
 import movie from './movie/Condo.mov?w=800&webp';
 
 export const GallerySection = () => {
-  const images = [
-    image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,
-    image11, image12, image13, image14, image15, image16, image17, image18, image19, image20,
-    image21, image22, image23, image24, image25, image26, image27, image28, image29, image30,
-    image31, image32, image33
+  const balcony = [
+    balcony1, balcony2, balcony3
+  ];
+
+  const living_room = [
+    lr1, lr2, lr3, lr4, lr5, lr6, lr7, lr8
+  ];
+
+  const kitchen = [
+    k1, k2, k3, k4, k5
+  ];
+
+  const bedrooms = [
+    br1, br2, br3, br4, br5
+  ];
+
+  const bathrooms = [
+    bt1, bt2, bt3
+  ];
+
+  const nature = [
+    n1, n2, n3
+  ];
+
+  const common = [
+    c1, c2
+  ];
+
+  const misc = [
+    m1, m2
   ];
 
   return (
-    <section className="px-8 pb-10 bg-base-200 shadow-xl" id="house_rules">
+    <section className="px-8 pb-10 bg-base-200 shadow-xl" id="gallery">
       <div className="mb-10 grid lg:ml-56 lg:mr-56">
-        <SectionHeader title="Gallery">
+        <SectionHeader title="Gallery" centerText={true}>
           <div className="mt-8 mb-8">
             <video className="h-auto max-w-full rounded-lg" autoPlay controls loop>
               <source src={movie} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
-          <div className="columns-1 md:columns-2 xl:columns-3 gap-7">
-            {images.map((image, index) => (
+        </SectionHeader>
+        <SectionHeader title="Living Room">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {living_room.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Balcony">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {balcony.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Kitchen">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {kitchen.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Bedrooms">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {bedrooms.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Bathrooms">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {bathrooms.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Miscellaneous">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {misc.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Common Areas">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {common.map((image, index) => (
+              <div className="break-inside-avoid mb-8">
+                <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
+              </div>
+            ))}
+          </div>
+        </SectionHeader>
+        <SectionHeader title="Nature">
+          <div className="columns-1 md:columns-2 xl:columns-3 gap-7 mt-8">
+            {nature.map((image, index) => (
               <div className="break-inside-avoid mb-8">
                 <img className="h-auto max-w-full rounded-lg" src={image} alt={"image " + (index + 1)} />
               </div>
